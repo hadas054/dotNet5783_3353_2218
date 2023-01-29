16d2a1d20aae7c423e103321f78686ca280c3cab
reut nisim <reutstudies@gmail.com>
2023-01-29 22:13:49 +0200

    level 6

--- BL/BO/OrderItem.cs
+++ BL/BO/OrderItem.cs
@@ -7,7 +7,7 @@ public class OrderItem
     public int ProductId { get; set; }
     public string? Name { get; set; }
     public double Price { get; set; }
-    public int InStock { get; set; }
+    public int Amount { get; set; }
     public double TotalPrice { get; set; }
     public override string ToString()
     {
